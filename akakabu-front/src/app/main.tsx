4559bfce29a9d27274e0a37c0e4ffc5c9e3dc380
赤江宇宙 <akaesora@akaeuchuunoMacBook-Pro.local>
2025-06-15 09:38:37 +0900

    必要ファイル追加

--- akakabu-front/src/app/main.tsx
+++ akakabu-front/src/app/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, useRoutes, HashRouter } from "react-router-dom";
 import { routes } from "./root"; // `root.ts` をインポート
+import './index.css';
 
 function App() {
   return useRoutes(routes); // ルートを適用
