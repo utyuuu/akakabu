92fd3cdbc78e0107fc249f86788b32b66f984971
赤江宇宙 <akaesora@akaeuchuunoMacBook-Pro.local>
2025-06-16 12:34:54 +0900

    postcss修正

--- akakabu-front/vite.config.ts
+++ akakabu-front/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import postcss from './postcss.config.js';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    postcss,
+  },
 });
