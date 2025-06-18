7aa81afb2ce186d4d115309d47b3e12655d0b557
赤江宇宙 <akaesora@akaeuchuunoMacBook-Pro.local>
2025-06-18 12:12:21 +0900

    vercel.json

--- akakabu-back/app/index.ts
+++ akakabu-back/app/index.ts
@@ -1,4 +1,4 @@
-import app from '../index.js'; // Express アプリを読み込む（←既存のファイル）
+import app from '../src/index.js'; // Express アプリを読み込む（←既存のファイル）
 import { Request, Response } from 'express';
 
 export default async function handler(req: Request, res: Response) {
