0f19c669a7e213728ea1016b68236ae6dd3a3f1e
赤江宇宙 <akaesora@akaeuchuunoMacBook-Pro.local>
2025-06-18 12:23:27 +0900

    app/index.tsの修正等

--- akakabu-back/app/index.ts
+++ akakabu-back/app/index.ts
@@ -1,6 +1,6 @@
 import app from '../src/index.js'; // Express アプリを読み込む（←既存のファイル）
-import { Request, Response } from 'express';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: Request, res: Response) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   app(req, res); // Express アプリを Vercel のリクエストに対応させる
 }
