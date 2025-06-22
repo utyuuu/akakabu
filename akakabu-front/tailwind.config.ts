7c0e5fd58be0c8722b466da49a7570842a2f132b
赤江宇宙 <akaesora@akaeuchuunoMacBook-Pro.local>
2025-06-22 17:50:58 +0900

    tailwind.configの修正

--- akakabu-front/tailwind.config.ts
+++ akakabu-front/tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   darkMode: ["class", '[data-mode="dark"]'],
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx,html}"
+    "./**/*.{html,js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
