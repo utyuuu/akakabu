a8b3e4b783985776df024e60eb2e67b6f690fa36
赤江宇宙 <akaesora@akaeuchuunoMacBook-Pro.local>
2025-06-22 21:18:32 +0900

    tailwindcss.cofig.ts変更

--- akakabu-front/tailwind.config.ts
+++ akakabu-front/tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./src/**/**/*.{html,js,jsx,ts,tsx}"
+    "./src/*.{html,js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
